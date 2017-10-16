2cc9f6af9d2291e3e198f8da0774891dc36b722b
Ariel Herman <aeh.herman@gmail.com>
2017-10-16 12:59:11 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index,:new,:create, :show]
+  # get '/student/:id', to: 'students#show', as: 'student'
+  # The above is the same as adding :show to the resources
 end
